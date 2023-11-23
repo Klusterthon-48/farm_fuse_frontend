e04561e6513f3ff2f0ae486157765f3e6e3bb646
olayinkagrace <olayinka4grace@gmail.com>
2023-11-23 11:10:06 +0100

    sidebar added

---
+++ app/dashbord/page.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import React, { useState } from 'react';
+import Sidebar from '../components/dashboard_components/Sidebar';
+import Navbar from '../components/dashboard_components/Navbar';
+import MainContent from '../components/dashboard_components/MainContent';
+
+
+export default function Dashboard () {
+  const [selectedItem, setSelectedItem] = useState('home');
+
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+  };
+
+  return (
+    <div className="flex">
+      <Sidebar selectedItem={selectedItem} onItemClick={handleItemClick} />
+      <div className="flex-1 bg-white">
+        <Navbar />
+        <MainContent selectedItem={selectedItem} />
+      </div>
+    </div>
+  );
+};
+
+
