add5e93cc19bf216b1fcf6e008c47a4177c1db53
Chibuokem Egbuchulam <talk2chibu@gmail.com>
2023-11-24 16:59:17 +0100

    feat: update dashboard

---
+++ app/components/utils/Schedule.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React from "react";
+
+const Schedule = ({ title, subtitle, data }) => {
+  return (
+    <div className="bg-accent-1 border-grey-4 rounded-lg p-8 my-3">
+      <h3 className="text-[20px] mb-[24px] font-medium">{title}</h3>
+      <h3 className="text-grey-3 text-[18px] mb-[17px]">{subtitle}:</h3>
+      <div className="flex flex-col gap-3">
+        {data.map((item) => (
+          <div key={item.crop} className="flex items-center gap-3 ">
+            <div>
+              <Image src={item.icon} alt="" width={15} height={15} />
+            </div>
+            <div>
+              <p className="font-medium">{item.crop}</p>
+            </div>
+            <div>
+              <p>{item.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
